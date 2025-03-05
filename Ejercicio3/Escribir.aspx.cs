019405877f5d58ba193d3d2f9b031644b713ffbb
EduarGZ <kevincarballo547@gmail.com>
2025-03-05 20:23:37 +0100

    Primer commit y push

---
+++ Ejercicio3/Escribir.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ejercicio3
+{
+    public partial class Escribir : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Confir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Confir_Click1(object sender, EventArgs e)
+        {
+            //Sava the session variables.
+            Session["ddlCategory"] = ddlCategory.SelectedValue;
+            Session["ddlSupplier"] = ddlSupplier.SelectedValue;
+            Session["txtProduct"] = txtProduct.Text;
+            Session["txtDescription"] = txtDescription.Text;
+            Session["txtImage"] = txtImage.Text;
+            Session["txtPrice"] = txtPrice.Text;
+            Session["txtNumberInStock"] = txtNumberInStock.Text;
+            Session["txtNumberOnOrder"] = txtNumberOnOrder.Text;
+            Session["txtReorderLevel"] = txtReorderLevel.Text;
+            //Go to the confirmation page.
+            Response.Redirect("Mostrar.aspx");
+        }
+    }
+}
